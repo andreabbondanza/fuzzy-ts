f3e8b999e59cd094491caf9506d16c8a20cecba8
Andrea Vincenzo Abbondanza <andreabbondanza.developer@outlook.com>
2020-10-12 10:31:21 +0200

    Package json created

--- dist/Fuzzy.js
+++ dist/Fuzzy.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("Hello!");
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Fuzzy = void 0;
+var Fuzzy = /** @class */ (function () {
+    function Fuzzy() {
+    }
+    return Fuzzy;
+}());
+exports.Fuzzy = Fuzzy;
